0214dca241dabbda25801c4809d836ac35b4e1ff
darkotosic <darkotosic1986@gmail.com>
2025-06-29 00:45:46 +0200

    Update countries.py

--- routers/countries.py
+++ routers/countries.py
@@ -1,10 +1,13 @@
+# routers/countries.py
+
 from fastapi import APIRouter
 from typing import List
+from models import Country
 from smartbets_API.api_football import get_countries
 
 router = APIRouter(prefix="/countries", tags=["countries"])
 
-@router.get("/", response_model=List[dict])
+@router.get("/", response_model=List[Country])
 async def read_countries():
-    data = await get_countries()
-    return data.get("response", [])
+    payload = await get_countries()
+    return payload.get("response", [])
